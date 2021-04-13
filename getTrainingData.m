3a42b01a56f08034381d62394424aa13def10ea4
Jean Chartier-Kastler <jean.chartierkastler@gmail.com>
2021-04-13 15:50:23 +0200

    update gitignore et ajout du code pour geznerer le training dataset

---
+++ getTrainingData.m
@@ -0,0 +1,44 @@
+% only upright test images in directory
+testFolder='pictures\'; %%%%% your test directory
+files=dir(testFolder);
+
+%get images
+clear filenames
+for i=3:size(files,1)
+    filenames{i-2,1}=files(i).name;
+end
+
+clear trainingInput
+clear trainingOutput
+trainingInput = [];
+trainingOutput = [];
+
+%generate train data
+for i=1:size(filenames,1)
+    disp(i + "/" + size(filenames,1));
+    I=imread(strcat(testFolder,(filenames{i})));
+    I = getTrainingImage(I);
+    
+    correctId=strsplit(filenames{i},'-');
+    correctId=str2double(correctId{1});
+
+    trainingInput=[trainingInput, I];
+    trainingOutput=[trainingOutput, correctId];
+
+    %A=[A,4];
+    
+end
+save('training','trainingInput','trainingOutput')
+
+%This function is going to make the image lighter from the basic image
+function [Image] = getTrainingImage(Image)
+    % input : I (upright raw RGB image)
+    % output : Image made for the training dataset
+    %get
+    Image = imresize(Image, [200 200]);
+
+end
+
+
+
+    
---
+++ trainDataset.m
@@ -0,0 +1,11 @@
+%init deep net
+% patternnet([5 10]) would create 2 hidden layers with 5 and 10 neurons
+%number of neurons : (Number of inputs + number of outputs) ^ 0.5 + (1 to 10)
+nbNeurons = (size(filenames,1) * 8) ^ 0.5;
+nbNeurons = nbNeurons - mod(nbNeurons, 10) %nbNeurons end by 0
+net = patternnet([nbNeurons/10 nbNeurons/10 nbNeurons/10 nbNeurons/5 nbNeurons/5 nbNeurons/10 nbNeurons/10 nbNeurons/10]);
+
+
+
+
+    
