724b0d63b8a16ef91914c0ce9f5f5551e11ae4c1
Jean Chartier-Kastler <jean.chartierkastler@gmail.com>
2021-04-13 18:50:18 +0200

    correction?

--- trainDataset.m
+++ trainDataset.m
@@ -3,12 +3,26 @@
 %number of neurons : (Number of inputs + number of outputs) ^ 0.5 + (1 to 10)
 nbNeurons = (407 * 8) ^ 0.5;
 nbNeurons = nbNeurons - mod(nbNeurons, 10) %nbNeurons end by 0
-net = patternnet([nbNeurons/10 nbNeurons/10 nbNeurons/10 nbNeurons/5 nbNeurons/5 nbNeurons/10 nbNeurons/10 nbNeurons/10]);
+net = [nbNeurons/10 nbNeurons/10 nbNeurons/10 nbNeurons/5 nbNeurons/5 nbNeurons/10 nbNeurons/10 nbNeurons/10];
 
-load('training','trainingInput','trainingOutput')
+layers = [ ...
+    imageInputLayer(net)
+    convolution2dLayer(5,20)
+    reluLayer
+    maxPooling2dLayer(2,'Stride',2)
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
+
+load('trainingGray','trainingInput','trainingOutput')
 size(trainingInput)
 size(trainingOutput)
 
-net=trainNetwork(trainingInput, trainingOutput, net);  
+options = trainingOptions('sgdm', ...
+    'MaxEpochs',20,...
+    'InitialLearnRate',1e-4, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+net=trainNetwork(trainingInput, trainingOutput, layers, options);  
 
 save('net', 'net');
