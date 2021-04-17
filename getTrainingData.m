da1c6ae557354f38330a6b07f7e6a16535f771af
Jean Chartier-Kastler <jean.chartierkastler@gmail.com>
2021-04-17 17:32:09 +0200

    corrections

--- getTrainingData.m
+++ getTrainingData.m
@@ -18,26 +18,26 @@ for i=1:size(filenames,1)
     disp(i + "/" + size(filenames,1));    
     I=imread(strcat(testFolder,(filenames{i})));
 
-    Height = size(I, 1);
-    Width = size(I, 2);
+    % Height = size(I, 1);
+    % Width = size(I, 2);
 
     I = getTrainingImage(I);
 
-    mkdir("trainingImages/"+i+"/")
-    
-    imwrite(I,"trainingImages/"+i+'/'+i+'.png')
     correctId=strsplit(filenames{i},'-');
     correctId=str2double(correctId{1});
+    mkdir("trainingImages/"+correctId+"/")
+    
+    imwrite(I,"trainingImages/"+correctId+'/'+correctId+'.png')
 
-    trainingInput = cat(3, trainingInput, I);
+    % trainingInput = cat(3, trainingInput, I);
     
     
-    trainingOutput=[trainingOutput, correctId];
-    size(trainingInput)
-    size(trainingOutput)
+    % trainingOutput=[trainingOutput, correctId];
+    % size(trainingInput)
+    % size(trainingOutput)
 
 end
-save('training','trainingInput','trainingOutput')
+% save('training','trainingInput','trainingOutput')
 
 %This function is going to make the image lighter from the basic image
 function [Image] = getTrainingImage(Image)
