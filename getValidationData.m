2863dbc094e847524d4c05b2502825f8add19a44
Jean Chartier-Kastler <jean.chartierkastler@gmail.com>
2021-04-17 18:00:00 +0200

    correction

--- getValidationData.m
+++ getValidationData.m
@@ -16,14 +16,16 @@ for i=1:size(filenames,1)
     type=strsplit(type{2},'.');
 
     type=str2double(type{1});
-    
+
     if(type == 2 | type == 3)
         
         disp(i + "/" + size(filenames,1));    
         
         I=imread(strcat(testFolder,(filenames{i})));
         
-        % I = getTrainingImage(I);
+
+
+        I = getValidationImage(I);
         correctId=strsplit(filenames{i},'-');
         
         correctId=str2double(correctId{1});
@@ -44,12 +46,10 @@ end
 % save('training','trainingInput','trainingOutput')
 
 %This function is going to make the image lighter from the basic image
-function [Image] = getTrainingImage(Image)
+function [Image] = getValidationImage(Image)
     % input : I (upright raw RGB image)
     % output : Image made for the training dataset
     %get
-
-    Image = getFaceCropped(Image);
     Image = imresize(Image, [224 224]);
     % Image = rgb2gray(Image);
     
